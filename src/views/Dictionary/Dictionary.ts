5559993f04874ced7312de788b8fdab06cc13d8f
Petr Grachev (petr9ra) <petr9ra@yandex.ru>
2022-02-05 20:19:24 +0300

    feat: add chapter buttons

--- src/views/Dictionary/Dictionary.ts
+++ src/views/Dictionary/Dictionary.ts
@@ -3,7 +3,32 @@ import { addElement, addTextElement } from '../../utils/add-element';
 
 export const Dictionary = (): HTMLElement => {
   const page = addElement('main', 'dictionary-page') as HTMLElement;
-  const pageTitle = addTextElement('h1', 'page-title', 'Dictionary Page') as HTMLElement;
+  const pageTitle = addTextElement('h1', 'page-title', 'Учебник') as HTMLElement;
+  const chapterList = addElement('div', 'chapter-list') as HTMLElement;
+
+  for (let i = 0; i < 6; i++) {
+    const chapterLabel = addElement('label', 'chapter-label') as HTMLLabelElement;
+    chapterLabel.setAttribute('for', `group-${i}`);
+    chapterLabel.textContent = `Часть ${i + 1}`;
+
+    const chapterRadio = document.createElement('input');
+    chapterRadio.type = 'radio';
+    chapterRadio.name = 'chapter';
+    chapterRadio.id = `group-${i}`;
+
+    if (i === 0) {
+      chapterRadio.checked = true;
+    }
+
+    const chapterWordsNums = addElement('p', 'chapter-words-nums') as HTMLSpanElement;
+    chapterWordsNums.textContent = `${i * 600 + 1}-${(i + 1) * 600}`;
+
+    chapterLabel.append(chapterWordsNums);
+    chapterList.append(chapterRadio);
+    chapterList.append(chapterLabel);
+  }
+
   page.append(pageTitle);
+  page.append(chapterList);
   return page;
 };
