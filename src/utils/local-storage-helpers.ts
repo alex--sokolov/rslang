3bdcd8e10329431476688c3beac962ca98e9b0df
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-02-07 02:29:27 +0300

    feat: game levels

--- src/utils/local-storage-helpers.ts
+++ src/utils/local-storage-helpers.ts
@@ -18,11 +18,17 @@ export const getToken = (type: string): string | null => {
 export const setUserId = (id: string): void => {
   localStorage.setItem('id', id);
 };
+export const setGameLevel = (level: string): void => {
+  localStorage.setItem('gameLevel', level);
+};
 export const setUserName = (name: string): void => {
   localStorage.setItem('username', name);
 };
 export const getUserId = (): string => {
   return <string>localStorage.getItem('ig');
+};
+export const getGameLevel = (): string => {
+  return <string>localStorage.getItem('gameLevel');
 };
 export const getUserName = (): string => {
   return <string>localStorage.getItem('username');
--- src/views/AudioCall/AudioCall.ts
+++ src/views/AudioCall/AudioCall.ts
@@ -1,9 +1,49 @@
 import './AudioCall.scss';
-import { addElement, addTextElement } from '../../utils/add-element';
+import { addElement } from '../../utils/add-element';
+import { getGameLevel, setGameLevel } from '../../utils/local-storage-helpers';
 
 export const AudioCall = (): HTMLElement => {
   const page = addElement('main', 'audio-call-page') as HTMLElement;
-  const pageTitle =  addTextElement('h1', 'page-title', 'Audio Call Page') as HTMLElement;
-  page.append(pageTitle);
+
+  page.innerHTML = `
+    <h2 class="audio-call-page__caption">АУДИОВЫЗОВ</h2>
+    <p class="audio-call-page__desc">Игра "Аудиовызов" улучшит ваше восприятие на слух и понимание устной речи.</p>
+    <div class="audio-call-page__difficulty difficulty">
+      <p class="difficulty__caption">Выберите сложность игры</p>
+      <ul id="audio-call-level" class="difficulty__list">
+        <li data-level="a1" class="difficulty__item">A1</li>
+        <li data-level="a2" class="difficulty__item">A2</li>
+        <li data-level="b1" class="difficulty__item">B1</li>
+        <li data-level="b2" class="difficulty__item">B2</li>
+        <li data-level="c1" class="difficulty__item">C1</li>
+        <li data-level="c2" class="difficulty__item">C2</li>
+      </ul>
+      <button class="difficulty__button">Начать</button>
+    </div>
+  `;
+
+  addListeners(page);
   return page;
+};
+
+function addListeners(element: HTMLElement) {
+  const levelsArea = element.querySelector('#audio-call-level') as HTMLUListElement;
+  const levels = element.querySelectorAll('.difficulty__item') as NodeListOf<HTMLElement>;
+  const activeLevel = getGameLevel();
+  levels.forEach((item: HTMLElement) => {
+    if (item.dataset.level === activeLevel) {
+      item.classList.add('active');
+    }
+  });
+  levelsArea.addEventListener('click', chooseLevel);
+
+  function chooseLevel(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    levels.forEach((item: HTMLElement) => item.classList.remove('active'));
+
+    if (target.dataset.level) {
+      setGameLevel(target.dataset.level);
+      target.classList.add('active');
+    }
+  }
 }
