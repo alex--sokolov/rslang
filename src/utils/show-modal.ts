3a0e6046c00c3a4e7ec90345ba8fdbddb11ef4f0
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-02-07 15:39:35 +0300

    feat: add modal window

---
+++ src/utils/show-modal.ts
@@ -0,0 +1,38 @@
+import './show-modal.scss';
+import { addElement, addTextElement } from './add-element';
+
+export const showModal = (element: HTMLElement): void => {
+
+  const overlay = document.querySelector('.overlay') as HTMLElement;
+  const overlayFadeInClass = 'overlay-fadeIn';
+  const removeModal = (modalEl: HTMLElement, overlayListener?: void) => {
+    modalEl.remove();
+    if (overlay.classList.contains(overlayFadeInClass)) overlay.classList.remove(overlayFadeInClass);
+    if (overlayListener) overlay.removeEventListener('click', overlayListener)
+  }
+  const modalClass = 'modal-window';
+  const modalOld = document.getElementById(modalClass);
+  if (modalOld) removeModal(modalOld);
+
+  const activeModalClass = 'modal-active';
+  const closeModalClass = 'modal-close';
+
+  const modal = addElement('div', modalClass, modalClass);
+  const closeModal = addTextElement('button', closeModalClass, 'Закрыть');
+  modal.classList.add(activeModalClass);
+  overlay.classList.add(overlayFadeInClass);
+
+  closeModal.addEventListener('click', () => {
+    removeModal(modal);
+  }, {once: true});
+
+  const overlayListener = overlay.addEventListener('click', () => {
+    removeModal(modal, overlayListener);
+  }, {once: true});
+
+  const root = document.getElementById('root') as HTMLDivElement;
+  modal.append(element);
+  modal.append(closeModal);
+  root.append(modal);
+
+};
