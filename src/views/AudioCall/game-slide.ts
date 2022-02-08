2104f7e03dd1edd4ed857736bd74723fe9800d57
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-02-08 21:26:21 +0300

    feat: audio of slides, switching slides, {game statistic}

--- src/views/AudioCall/game-slide.ts
+++ src/views/AudioCall/game-slide.ts
@@ -3,36 +3,42 @@ import { Word } from '../../interfaces';
 import { baseUrl } from '../../components/api/api';
 import soundIcon from '../../assets/img/sound.svg';
 
-function getSlide(param: Word, type?: string): HTMLElement {
+function getSlide(param: Word, ans: Array<Word>, type?: string): HTMLElement {
   const slide = addElement('div', 'audio-call-slide slide') as HTMLDivElement;
+  slide.setAttribute('data-id', param.id);
   if (type) slide.classList.add('hide');
 
   slide.innerHTML = `
+    <audio class="slide__audio-element" src=${baseUrl + param.audio}></audio>
     <div class="slide__question">
       <div class="slide__image">
         <img src=${baseUrl + param.image} alt="word image">
       </div>
       <div class="slide__sound">
         <span>${param.word}</span>
-        <img src=${soundIcon} id="audio-game-sound" alt="play word">
+        <img src=${soundIcon} class="audio-game-sound" alt="play word">
       </div>
     </div>
     <div class="slide__answers">
       <div class="slide__answers-row">
-        <span class="slide__answers__item">rgsjhsgh</span>
-        <span class="slide__answers__item">sdfghsgdfh</span>
-        <span class="slide__answers__item">fghfh</span>
+        <span data-id=${ans[0].id} class="slide__answers__item">${ans[0].wordTranslate}</span>
+        <span data-id=${ans[1].id} class="slide__answers__item">${ans[1].wordTranslate}</span>
+        <span data-id=${ans[2].id} class="slide__answers__item">${ans[2].wordTranslate}</span>
       </div>
       <div class="slide__answers-row">
-        <span class="slide__answers__item">fghfgh</span>
-        <span class="slide__answers__item">fghfg</span>
-        <span class="slide__answers__item">fghfghrt</span>
+        <span data-id=${ans[3].id} class="slide__answers__item">${ans[3].wordTranslate}</span>
+        <span data-id=${ans[4].id} class="slide__answers__item">${ans[4].wordTranslate}</span>
+        <span data-id=${ans[5].id} class="slide__answers__item">${ans[5].wordTranslate}</span>
       </div>
     </div>
-    <button disabled class="slide__button" id="audio-game-button">Дальше</button>
+    <button disabled class="slide__button audio-game-button">Дальше</button>
   `;
 
   return slide;
 }
 
-export { getSlide };
+function getEmptySlide(): HTMLElement {
+  return addElement('div', 'audio-call-slide slide hide') as HTMLElement;
+}
+
+export { getSlide, getEmptySlide };
