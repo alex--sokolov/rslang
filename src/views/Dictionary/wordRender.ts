f615ecd36e97354f7f6b65f51396c74f5676267b
Petr Grachev (petr9ra) <petr9ra@yandex.ru>
2022-02-11 01:42:03 +0300

    feat: add footer, styles for header and footer and DictionaryPage (partly)

--- src/views/Dictionary/wordRender.ts
+++ src/views/Dictionary/wordRender.ts
@@ -9,7 +9,7 @@ export const wordCardRender = (word: Word): HTMLDivElement => {
   const btnsContainer = addElement('div', 'card__btns-container') as HTMLDivElement;
 
   const image =`<img src="https://rs-lang-app-server.herokuapp.com/${word.image}" class="card__img" alt="Иллюстрация слова">`;
-  const wordEng = addTextElement('h3', 'card__word', `${word.word}`) as HTMLHeadingElement;
+  const wordEng = addTextElement('h3', 'card__word', `${word.word} - `) as HTMLHeadingElement;
   const transcription = addTextElement('span', 'card__transcription', `${word.transcription}`) as HTMLSpanElement;
   const translate = addTextElement('h4', 'card__translate', `${word.wordTranslate}`) as HTMLParagraphElement;
   const subheading = addTextElement('h5', 'card__subheading', 'Значение') as HTMLHeadingElement;
@@ -19,15 +19,15 @@ export const wordCardRender = (word: Word): HTMLDivElement => {
     'p',
     'card__text',
     `${word.textMeaningTranslate}`
-    ) as HTMLParagraphElement;
+  ) as HTMLParagraphElement;
   const subheading2 = addTextElement('h5', 'card__subheading', 'Пример') as HTMLHeadingElement;
   const textExample = addElement('p', 'card__text') as HTMLParagraphElement;
   textExample.insertAdjacentHTML('beforeend', word.textExample);
   const textExampleTranslate = addTextElement(
-      'p',
-      'card__text',
-      `${word.textExampleTranslate}`
-      ) as HTMLParagraphElement;
+    'p',
+    'card__text',
+    `${word.textExampleTranslate}`
+  ) as HTMLParagraphElement;
   const voiceBtn = addElement('button', 'card__voice-btn') as HTMLButtonElement;
 
   const wordCheckboxes = `
@@ -50,10 +50,10 @@ export const wordCardRender = (word: Word): HTMLDivElement => {
     btnsContainer
   );
 
-  wordCardLeftSide.insertAdjacentHTML("afterbegin", image);
+  wordCardLeftSide.insertAdjacentHTML('afterbegin', image);
   btnsContainer.append(voiceBtn);
   // if (getUserId()) // TODO: Turn on
-  btnsContainer.insertAdjacentHTML("beforeend", wordCheckboxes);
+  btnsContainer.insertAdjacentHTML('beforeend', wordCheckboxes);
 
   wordCardContainer.append(wordCardLeftSide, wordCardRightSide);
 
