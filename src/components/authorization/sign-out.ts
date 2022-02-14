46adad2669274438fdf7fea4c66d97b1cd4ca219
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-02-14 22:09:23 +0300

    fix: reload after sign out

--- src/components/authorization/sign-out.ts
+++ src/components/authorization/sign-out.ts
@@ -1,4 +1,5 @@
 import { openAuthModal } from './authorization';
+import { navigate } from '../../engine/router-hash';
 
 function signOut() {
   const headerButton = document.querySelector('.navbar-auth') as HTMLFormElement;
@@ -9,6 +10,7 @@ function signOut() {
   headerName.innerText = '';
   headerButton.removeEventListener('click', signOut);
   headerButton.addEventListener('click', openAuthModal);
+  navigate();
 }
 
 export { signOut };
