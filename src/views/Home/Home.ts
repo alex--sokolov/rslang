296725feb1611cf55b47968012f98cb0dd344794
Petr Grachev (petr9ra) <petr9ra@yandex.ru>
2022-02-19 18:06:54 +0300

    feat: add image for home page

--- src/views/Home/Home.ts
+++ src/views/Home/Home.ts
@@ -3,19 +3,42 @@ import { addElement, addTextElement } from '../../utils/add-element';
 
 export const Home = (): HTMLElement => {
   const home = addElement('main', 'home-page container') as HTMLElement;
+  const contentBlock = addElement('div', 'home-page__content-block') as HTMLDivElement;
   const pageTitle = addTextElement('h1', 'page-title home-page__logo', 'RS Lang') as HTMLElement;
 
-  const mainDescription = addTextElement('p', 'home-page__main-description', 'Это приложение поможет вам освоить 3600 наиболее часто употребляемых слов английского языка.');
+  const mainDescription = addTextElement(
+    'p',
+    'home-page__main-description',
+    'Это приложение поможет вам освоить 3600 наиболее часто употребляемых слов английского языка.'
+  );
   const featureTitle = addTextElement('p', 'home-page__feature-title', 'В наличии:');
 
   const featureList = `
-    <p class="home-page__feature-item"><a class="home-page__link" href="/dictionary">Учебник</a>, в котором ты можешь просматировать все слова, слушать произношение, и отмечать слова как сложные или изученные.<br></p>
-    <p class="home-page__feature-item">Игра <a class="home-page__link" href="/gameSprint">Спринт</a>, где необходимо выбрать правильное слово из двух предложенных на время.<br></p>
-    <p class="home-page__feature-item">Игра <a class="home-page__link" href="/gameAudioCall">Аудиовызов</a>, в которой необходимо выбрать произнесённое слово из шести предложенных вариантов.</p>
-    <p class="home-page__feature-item"><a class="home-page__link" href="/stat">Статистика</a>, в которой ты можешь увидеть прогресс своего обучения.</p>
+    <p class="home-page__feature-item">
+      <a class="home-page__link" href="/dictionary">Учебник</a>,
+      в котором ты можешь просматировать все слова, слушать произношение, и отмечать слова как сложные или изученные.
+    </p>
+    <p class="home-page__feature-item">Игра
+      <a class="home-page__link" href="/gameSprint">Спринт</a>,
+      где необходимо выбрать правильное слово из двух предложенных на время.
+      </p>
+    <p class="home-page__feature-item">Игра
+      <a class="home-page__link" href="/gameAudioCall">Аудиовызов</a>,
+      в которой необходимо выбрать произнесённое слово из шести предложенных вариантов.
+    </p>
+    <p class="home-page__feature-item">
+      <a class="home-page__link" href="/stat">Статистика</a>,
+      в которой ты можешь увидеть прогресс своего обучения.
+    </p>
   `;
 
-  home.append(pageTitle, mainDescription, featureTitle);
-  home.insertAdjacentHTML('beforeend', featureList);
+  const imageBlock = `<img class="home-page__img" src="../../assets/img/home-page-bg.jpg" alt="Просто картинка">`;
+
+
+  contentBlock.append(pageTitle, mainDescription, featureTitle);
+  contentBlock.insertAdjacentHTML('beforeend', featureList);
+
+  home.append(contentBlock);
+  home.insertAdjacentHTML('beforeend', imageBlock);
   return home;
 };
