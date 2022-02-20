9c929f1c971bbafc1a617adfb7add2880f4eefbe
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-02-20 19:55:11 +0300

    feat: header burger

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -2,11 +2,12 @@ import './header.scss';
 import { addElement } from '../../utils/add-element';
 import { NavBar } from '../navbar/navbar';
 import { AuthPanel } from '../authorization/authorization';
+import { Burger } from '../burger/burger';
 
 const Header = () => {
   const element = addElement('header', 'header');
   const container = addElement('div', 'container') as HTMLDivElement;
-  container.append(NavBar(), AuthPanel());
+  container.append(NavBar(), Burger(), AuthPanel());
   element.append(container);
   return element;
 };
